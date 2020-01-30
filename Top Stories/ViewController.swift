aa8f9ab7374caf94ed5f2c00cb105109021dbcae
Brodi <bbloss@bsd220.org>
2020-01-30 10:34:55 -0600

    Initial Commit

---
+++ Top Stories/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Top Stories
+//
+//  Created by Brodi Bloss on 1/30/20.
+//  Copyright © 2020 Brodi Bloss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
